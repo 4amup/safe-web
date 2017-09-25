33a62a6fa0317ff1bd2da14eaaff1d29e737328a
lixiaochun <490398311@qq.com>
2017-09-25 22:49:48 +0800

    done 实现了查询功能

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Trouble = require('../db');
+
+// 在数据库中查询
+(async () => {
+  var troubles = await Trouble.findAll();
+  console.log(troubles.length);
+
+  let trouble = troubles[troubles.length - 1];
+
+})();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '开发中的首页', troubleTitle:'troubletitle', imagePath: 'example/image-1505528928463.jpg', imageDescription:'问题的描述'});
+  Trouble.findAll().then(troubles => {
+    if (troubles.length === 0) {
+      res.render('index', {title: '默认', imageDescription: '默认', imagePath: `example/example.jpg`, Lng:45.7137085949, Lat:126.6769766808})
+    } else {
+      var trouble = troubles[troubles.length - 1];
+      var path = trouble.imagePath.split('\\').splice(1, 3).join('/');
+      res.render('index', { title: '开发中的首页', imageDescription: trouble.troubleDescription, imagePath: path, Lng: trouble.Lng, Lat: trouble.Lat});      
+    }
+  })
 });
 
 module.exports = router;
--- app/routes/upload.js
+++ app/routes/upload.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var multer = require('multer');
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/public/example')
+    cb(null, 'public/example')
   },
   filename: function (req, file, cb) {
     var fileFormat = (file.originalname).split(".");
@@ -17,8 +17,17 @@ var Trouble = require('../db')
 
 // 解析post请求，将解析内容写入数据库
 router.post('/',upload.single('image'), function(req, res, next) {
-  Trouble.create({ 'imagePath': req.file.path+'.jpg', 'troubleDescription': req.body.description});
+  var location = genLacationInfo([45.710540, 126.672342], [45.718960, 126.682878]);
+  // 处理一下path，将\处理成\/后返回
+  Trouble.create({ 'imagePath': req.file.path, 'troubleDescription': req.body.description, 'Lng': location[0], 'Lat': location[1]});
   res.redirect('/');
 });
+
+// 模拟一个随机生成经纬度的函数
+function genLacationInfo (x, y) { // x代表西南角坐标，y代表东北角坐标，形式是[Lng, Lat]，[纬度, 经度]
+  var Lng = x[0] + Math.random() * (y[0] - x[0]);
+  var Lat = x[1] + Math.random() * (y[1] - x[1]);
+  return [Lng, Lat];
+}
 
 module.exports = router;
