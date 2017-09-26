a9800c72ee60078c980f667365f1b39975b77fed
lixiaochunoffice <490398311@qq.com>
2017-09-26 08:11:53 +0800

    fixed 将首页的查询逻辑修正

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,25 +2,17 @@ var express = require('express');
 var router = express.Router();
 var Trouble = require('../db');
 
-// 在数据库中查询
-(async () => {
-  var troubles = await Trouble.findAll();
-  console.log(troubles.length);
-
-  let trouble = troubles[troubles.length - 1];
-
-})();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // 数据库中查询出内容
   Trouble.findAll().then(troubles => {
-    if (troubles.length === 0) {
-      res.render('index', {title: '默认', imageDescription: '默认', imagePath: `example/example.jpg`, Lng:45.7137085949, Lat:126.6769766808})
-    } else {
-      var trouble = troubles[troubles.length - 1];
-      var path = trouble.imagePath.split('\\').splice(1, 3).join('/');
-      res.render('index', { title: '开发中的首页', imageDescription: trouble.troubleDescription, imagePath: path, Lng: trouble.Lng, Lat: trouble.Lat});      
-    }
+    var trouble = troubles[troubles.length - 1];
+    var path = trouble.imagePath.split('\\').splice(1, 3).join('/');
+    res.render('index', { title: '开发中的首页', imageDescription: trouble.troubleDescription, imagePath: path, Lng: trouble.Lng, Lat: trouble.Lat});
+  })
+  .catch(error => {
+    console.log('数据库查询结果为空，显示默认内容')
+    res.render('index', {title: '默认', imageDescription: '默认', imagePath: `example/example.jpg`, Lng:45.7137085949, Lat:126.6769766808})
   })
 });
 
