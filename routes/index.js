47e5dec4cc6124954e13027b76c1a56d704468f0
lixiaochunoffice <lixiaochun0129@163.com>
2017-10-13 19:09:09 +0800

    fix 删除index.js路由的无效multer部分代码

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,5 @@
 var express = require('express');
 var router = express.Router();
-// 文件上传中间件配置
-var multer = require('multer');
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/example')
-  },
-  filename: function (req, file, cb) {
-    var fileFormat = (file.originalname).split(".");
-    cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
-  }
-})
-var upload = multer({ storage: storage }); // 规定文件储存位置
-
-// 引入模型
-var Trouble = require('../db')
 
 // 首页get请求
 router.get('/', function(req, res, next) {
