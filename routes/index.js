f537d62dda087556833a43600902d8589a02e5d0
lixiaochun <490398311@qq.com>
2017-10-19 09:14:55 +0800

    update 加上了左侧栏，与我相关

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var Trouble = require('../db');
+
 
 // 首页get请求
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '开发中的首页' });
+  Trouble.findAll({
+    attributes: ['id', 'imageDescription', 'imagePath', 'Lng', 'Lat']
+  })
+  .then(troubles => {
+    if (troubles.length) {
+      // troubles
+      console.log('有查询内容，正常渲染');
+    } else {
+      console.log('数据库查询结果为空，显示默认内容');
+      // 默认数据格式
+      var trouble = {
+        imageDescription: '无数据，显示默认内容',
+        imagePath: 'example/example.jpg',
+        Lng: 45.716503,
+        Lat: 126.678114
+      }
+      troubles.push(trouble);
+    }
+    // 将处理后的数据传输到前端页面
+    res.render('index', { title: '开发中的首页', troubles: troubles});
+  })
+  
 });
 
 module.exports = router;
