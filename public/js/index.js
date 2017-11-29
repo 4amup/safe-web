dc4acb421e9eeb6dbb1a95824ab9715fb8ad1487
lixiaochunoffice <lixiaochun0129@163.com>
2017-11-29 20:11:51 +0800

    update 实现了单位的联动，下一步明确自定义位置

--- public/js/index.js
+++ public/js/index.js
@@ -10,9 +10,10 @@ $(function () {
     }
   });
 
+  var center = [126.677706, 45.714858];
   // 定义公司范围，随后通过ajax绘制后异步显示地图
   var map = new AMap.Map('map', {
-    center: [126.677706, 45.714858],
+    center: center,
     zoom: 16,
     rotateEnable: true,
     resizeEnable: true,
@@ -23,12 +24,15 @@ $(function () {
   // 定义临时marker
   var tempTroubleMarker = new AMap.Marker({
     icon: new AMap.Icon({
-        size: new AMap.Size(20, 20),  //图标大小
+        size: new AMap.Size(36, 36),  //图标大小
         image: "../images/new.png",
-        // imageOffset: new AMap.Pixel(0, -60),
+        imageOffset: new AMap.Pixel(0, 5),
         imageSize: new AMap.Size(20, 20)
     }),
-    animation: 'AMAP_ANIMATION_BOUNCE',
+    // animation: 'AMAP_ANIMATION_BOUNCE',
+    cursor: 'move',
+    // draggable: true,
+    // raiseOnDrag: true
   });
 
   // 定义公司
@@ -39,6 +43,7 @@ $(function () {
     strokeWeight: 1,
     fillColor: "#f5deb3",
     fillOpacity: 0.35,
+    bubble: true,
   });
   companyPolygon.areas = [];
 
@@ -72,9 +77,22 @@ $(function () {
         strokeWeight: 1,
         fillColor: "#f5dbe3",
         fillOpacity: 0.35,
-        id: value.id
+        // bubble: true,
+      });
+      areaPolygon.setExtData({
+        id: value.id,
+        name: value.name
       });
       $('select[name="troubleArea"]').append($('<option></option').text(value.name));
+      // 绑定事件，自动识别单位信息
+      areaPolygon.on('click', function(e) {
+        var text = e.target.getExtData().name;
+        $('select option').removeAttr('selected');
+        $(`select option:contains(${text})`).attr('selected', 'selected');
+        tempTroubleMarker.setPosition(e.lnglat);
+        tempTroubleMarker.setMap(map);
+        map.setCenter(e.lnglat);
+      });
       companyPolygon.areas.push(areaPolygon);
     });
   })
@@ -118,13 +136,19 @@ $(function () {
       if (_gpsLat && _gpsLng) { // 如果有经纬度，将经纬度信息换算成小数模式后，写入定位输入框内
         var Lat = _gpsLat[0] + (_gpsLat[1]+_gpsLat[2]/60)/60;
         var Lng = _gpsLng[0] + (_gpsLng[1]+_gpsLng[2]/60)/60;
-        // 使用坐标转行工具将gps坐标转换成高德坐标
-        AMap.convertFrom(Lng+','+Lat, 'gps', function(status, result) {
-          console.log(status);
-          var location = [result.locations[0].lng, result.locations[0].lat];
-          tempTroubleMarker.setPosition(location);
-          tempTroubleMarker.setMap(map);
-        });
+
+        // 如果还没有由用户标记位置，则由gps信息定位位置，否则保留用户定义的位置
+        if(!tempTroubleMarker.getPosition()) {
+          // 使用坐标转行工具将gps坐标转换成高德坐标
+          AMap.convertFrom(Lng+','+Lat, 'gps', function(status, result) {
+            console.log(status);
+            var location = [result.locations[0].lng, result.locations[0].lat];
+            tempTroubleMarker.setPosition(resetLocation(location));
+            tempTroubleMarker.setMap(map);
+            map.setZoom(18);
+            map.setCenter(resetLocation(location));
+          });
+        }
       } else {
         // to-do
         // 将来可以添加到页面提示信息栏目
@@ -132,4 +156,80 @@ $(function () {
       }
     });
   });
+
+  map.on('click', function(e) {
+    tempTroubleMarker.setPosition(resetLocation(e.lnglat));
+    tempTroubleMarker.setMap(map);
+    map.setZoom(18);
+    map.setCenter(resetLocation(e.lnglat));
+    $('select option').removeAttr('selected');
+    $(`select option:contains("未定义地点")`).attr('selected', 'selected');
+  });
+
+  // 工具函数，判断点是否在公司范围内
+  function resetLocation(location) {
+    // 先判断location是对象还是数组
+    var l;
+    if(Array.isArray(location)) { // 如果是数组
+      l = location;
+    } else {
+      l = [location.lng, location.lat];
+    }
+    // 再判断点是否在公司范围内，如不在重置为中心
+    if(companyPolygon.contains(l)) {
+      return l;
+    } else {
+      return center;
+    }
+  }
+
+  $('select[name="troubleArea"]').on('change', function(e) {
+    var areaName = $(this).val();
+    if(areaName === '未定义地点') {
+      tempTroubleMarker.setPosition(center);
+      tempTroubleMarker.setMap(map);
+      map.setCenter(center);
+    }
+    companyPolygon.areas.forEach(function(value, index) {
+      if(value.getExtData().name === areaName) {
+        //
+        tempTroubleMarker.setPosition(polygonCenter(value));
+        tempTroubleMarker.setMap(map);
+        map.setCenter(polygonCenter(value));
+      }
+    });
+  });
+
+  // 多边形中心公式
+  function polygonCenter (polygon) {
+    var array = polygon.getPath()
+    var lng = 0
+    var lat = 0;
+    array.forEach(function(value, index) {
+      lng+=value.lng;
+      lat+=value.lat;
+    });
+    return [lng/array.length, lat/array.length];
+  }
+
+  // // 拖动临时点移动完后，重新判断select
+  // tempTroubleMarker.on('dragend', function(e) {
+  //   var location = e.lnglat;
+  //   map.emit('click', e);
+  // });
+
+  // 异步提交待开发
+  // $('#form').submit(function(e) {
+  //   e.preventDefault();
+  //   var form = $(this);
+  //   var troubleMarkerPosition = tempTroubleMarker.getPosition().map(function(value, index) {
+  //     return [value.lng, value.lat];
+  //   });
+
+  //   $.ajax({
+  //     url:form.attr('action'),
+  //     type: form.attr('method'),
+  //     data: form.serialize() + 'Markerposition' + JSON.stringify(troubleMarkerPosition);
+  //   })
+  // })
 });
