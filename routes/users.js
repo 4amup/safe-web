6db15e0daec21e0cce76917d8e7df064e5ea1ec6
lixiaochunoffice <lixiaochun0129@163.com>
2017-12-27 16:54:24 +0800

    add 用户信息页完成

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var model = require('../model');
+var User = model.User;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:user_id', function(req, res, next) {
+  User.findById(req.params.user_id)
+  .then(function(user) {
+    res.render('user', {
+      title: `${user.name}的个人主页`,
+      user: user,
+      locals: res.locals
+    });
+  });
 });
 
 module.exports = router;
