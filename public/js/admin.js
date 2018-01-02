3fa3e4d1be2a8a0cb79eaf41a3af49706a854ffa
lixiaochunmac <490398311@qq.com>
2018-01-03 06:46:27 +0800

    update 二级合并查找效率高，再高级就适合分开查找了

--- public/js/admin.js
+++ public/js/admin.js
@@ -88,14 +88,30 @@ $(function() {
     map.setMapStyle('amap://styles/grey'); // 设置地图特殊样式，提示可以开始划范围了
 
     // 将原文copy到修改的表单中
-    editForm.children('input:first-child').val($('.companyShow').find('span:eq(0)').text());
-    editForm.children('input:last-child').val($('.companyShow').find('span:eq(1)').text());
+    var info = $(ev.target).prev();
+    var name = info.prev();
+    editForm.children('input[type="text"]:eq(0)').val(name.text());
+    editForm.children('input[type="text"]:eq(1)').val(info.text());
 
-    companyPolygon.setOptions({
-      strokeStyle: 'dashed'
-    });
-
-    editingPolygon = new AMap.PolyEditor(map,companyPolygon); // 接着把绘制的多边形变成可编辑状态
+    // 通过id让将目标的polygon变成可编辑状态
+    var targetPolygon;
+    var polygonId = $(ev.target).parent().attr('id');
+    if (companyPolygon.getExtData('id') === polygonId) {    
+      companyPolygon.setOptions({
+        strokeStyle: 'dashed'
+      });
+      targetPolygon = companyPolygon;
+    }else {
+      companyPolygon.areas.forEach(function(value, index) {
+        if(value.getExtData('id') === polygonId) {
+          value.setOptions({
+            strokeStyle: 'dashed'
+          });
+          targetPolygon = value;
+        }
+      })
+    }
+    editingPolygon = new AMap.PolyEditor(map,targetPolygon); // 接着把绘制的多边形变成可编辑状态
     editingPolygon.open();
     $('.buttonBox').show();
     // 添加编辑事件按钮事件
@@ -107,6 +123,9 @@ $(function() {
       editingPolygon.close();
     }, false);
   });
+
+  // 取消编辑现有范围
+
 
   // 编辑部门消息按钮
   $('.companyBox').on('click', '.editArea', function(ev) {
@@ -120,16 +139,16 @@ $(function() {
     // 自动弹出编辑范围
     areaIndex = $('.areaBox h5').index(areaItem); // area当前索引
     map.setMapStyle('amap://styles/blue'); // 设置地图特殊样式，提示可以开始划范围了
-    editAreaPolygon = new AMap.PolyEditor(map,companyPolygon.areas[areaIndex]); // 接着把绘制的多边形变成可编辑状态
-    editAreaPolygon.open();
+    editingPolygon = new AMap.PolyEditor(map,companyPolygon.areas[areaIndex]); // 接着把绘制的多边形变成可编辑状态
+    editingPolygon.open();
     $('.buttonBox').show();
     // 添加编辑事件按钮事件
     AMap.event.addDomListener(document.getElementById('editPolygon'), 'click', function() {
-      editAreaPolygon.open();
+      editingPolygon.open();
     }, false);
     // 添加结束编辑事件按钮事件
     AMap.event.addDomListener(document.getElementById('overPolygon'), 'click', function() {
-      editAreaPolygon.close();
+      editingPolygon.close();
     }, false);
   });
 
@@ -174,6 +193,7 @@ $(function() {
       companyPolygon.setExtData({id:company.id});
       // 前端显示刚上传的数据
       $('.companyShow h3').show();
+      $('.companyShow h3').attr('id', company.id);
       $('.areaBox').show();
       $('#createCompany').hide();
       $('.control_add:first-child').hide();
@@ -203,11 +223,13 @@ $(function() {
     })
     .done(function(company) {
       console.log(`公司信息${form.attr('method')}成功`);
-      console.log(company);
-      companyPolygon.id = company.id;
-      editCompanyPolygon.close();
+      companyPolygon.setExtData({id:company.id});
+      editingPolygon.close();
       var path = JSON.parse(company.polygonPath);
       companyPolygon.setPath(path);
+      companyPolygon.setOptions({
+        strokeStyle: 'solid'
+      });
       // 前端显示刚上传的数据。
       $('.companyBox span:eq(0)').text(company.name);
       $('.companyBox span:eq(1)').text(company.info);
@@ -265,8 +287,10 @@ $(function() {
         strokeWeight: 1,
         fillColor: "#f5dbe3",
         fillOpacity: 0.35,
-        id: value.id
       });
+      areaPolygon.strokeStyle({
+        id: value.id
+      })
       companyPolygon.areas.push(areaPolygon);
     });
   })
@@ -302,20 +326,23 @@ $(function() {
         strokeWeight: 1,
         fillColor: "#f5dbe3",
         fillOpacity: 0.35,
+      });
+      areaPolygon.setExtData({
         id: area.id,
         name: area.name
-      });
+      })
       companyPolygon.areas.push(areaPolygon);
       // 前端显示刚上传的数据。
-      var h5 = $('<h5></h5>');
-      h5.attr('class', 'areaShow').attr('id', area.id);
-      h5.html(`
+      var h3 = $('<h3></h3>');
+      h3.attr('class', 'areaShow').attr('id', area.id);
+      h3.html(`
       <span>${ area.name }</span>
       <span>${ area.info }</span>
-      <a href="" class="editArea">edit</a>
-      <a href="api/company/areas/${area.id}" class="delete">del</a>
+      <a href="#" class="editPolygon">编辑</a>
+      <a href="#" class="cancelPolygon">取消</a>
+      <a href="api/company/areas/${area.id}" class="delete">删除</a>
       `);
-      $('.areaBox').append(h5);
+      $('.areaBox').append(h3);
       $('#createArea input[type="text"]').val(null);
       map.setMapStyle('amap://styles/normal'); // 恢复地图正常样式
       $('.buttonBox').hide();
